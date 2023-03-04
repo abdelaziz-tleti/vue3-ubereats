5dce5fd9403f24218da31e95d8583dfae2eeb8dd
konan <abdelaziz.git@gmail.com>
2023-03-04 21:44:31 +0100

    create component RestaurantCard

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <template>
 <div id="app"></div>
-aaaa
+<RestaurantCard/>
+<RestaurantCard/>
+<RestaurantCard/>
 </template>
-
 <script>
-
+import RestaurantCard from './components/RestaurantCard.vue';
 export default {
   name: 'App',
- 
+  components:{
+    RestaurantCard
+  }
 }
 </script>
 
+
 <style>
+@import url('./style/reset.css');
+@import url('https://fonts.googleapis.com/css2?family=Roboto%3Awght%40300%3B400%3B700&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Roboto', sans-serif;
 }
 </style>
